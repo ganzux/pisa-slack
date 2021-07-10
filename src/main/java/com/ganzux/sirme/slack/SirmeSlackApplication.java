5307291ad2059c59c7ef30c48949cfe867dca1e2
alvaro <alvaro.alcedomoreno@optum.com>
2021-07-10 12:49:25 +0100

    Test property

--- src/main/java/com/ganzux/sirme/slack/SirmeSlackApplication.java
+++ src/main/java/com/ganzux/sirme/slack/SirmeSlackApplication.java
@@ -1,12 +1,19 @@
 package com.ganzux.sirme.slack;
 
+import com.ganzux.sirme.slack.controller.WelcomeController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SirmeSlackApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(SirmeSlackApplication.class.getCanonicalName());
+
 	public static void main(String[] args) {
+		LOGGER.info("Initializing SpringBoot app...");
 		SpringApplication.run(SirmeSlackApplication.class, args);
 	}
 
--- src/main/java/com/ganzux/sirme/slack/controller/WelcomeController.java
+++ src/main/java/com/ganzux/sirme/slack/controller/WelcomeController.java
@@ -1,5 +1,8 @@
 package com.ganzux.sirme.slack.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,11 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
-    StringBuilder sb = new StringBuilder("Hello World");
+    private static final Logger LOGGER = LoggerFactory.getLogger(WelcomeController.class.getCanonicalName());
+    private static StringBuilder sb = new StringBuilder("Hello World");
+
+    @Value("${test.prop}")
+    String TEST_PROP;
 
     @GetMapping("/")
     public String welcome() {
         sb.append("!");
+
+        LOGGER.info(sb.toString());
+        LOGGER.info(TEST_PROP);
+
         return sb.toString();
     }
 
