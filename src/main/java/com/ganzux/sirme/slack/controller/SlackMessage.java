a78e5f39b2fa65f17947ecb714089ff3062d0fe9
alvaro <alvaro.alcedomoreno@optum.com>
2021-07-10 14:26:30 +0100

    Exception for message

--- src/main/java/com/ganzux/sirme/slack/controller/SlackMessage.java
+++ src/main/java/com/ganzux/sirme/slack/controller/SlackMessage.java
@@ -1,12 +1,10 @@
 package com.ganzux.sirme.slack.controller;
 
-import com.slack.api.Slack;
+import com.ganzux.sirme.slack.exceptions.MessageException;
 import com.slack.api.bolt.App;
-import com.slack.api.methods.MethodsClient;
 import com.slack.api.methods.request.chat.ChatPostMessageRequest;
 import com.slack.api.methods.response.chat.ChatPostMessageResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -20,17 +18,20 @@ public class SlackMessage {
     @Autowired
     App slackApp;
 
+
     public void rootMessage(String originalAddress) throws Exception {
 
         ChatPostMessageRequest request = ChatPostMessageRequest.builder()
                 .channel("#general")
                 .text(":thinking_face: Hey! " + originalAddress + " is looking under the table!")
+                .token(slackApp.config().getSingleTeamBotToken())
                 .build();
 
-        // Get a response as a Java object
         ChatPostMessageResponse response = slackApp.client().chatPostMessage(request);
 
-        System.out.println(response);
+        if (!response.isOk()) {
+            throw new MessageException("Error sending message, no response " + response.getError());
+        }
     }
 
 }
