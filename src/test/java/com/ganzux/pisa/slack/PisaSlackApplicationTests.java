2789d3be3823f72bf8c614aef2ed40d73c0dab44
alvaro <alvaro.alcedomoreno@optum.com>
2021-07-15 08:52:10 +0100

    New name, PISA

--- src/test/java/com/ganzux/pisa/slack/PisaSlackApplicationTests.java
+++ src/test/java/com/ganzux/pisa/slack/PisaSlackApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SirmeSlackApplicationTests {
+class PisaSlackApplicationTests {
 
 	@Test
 	void contextLoads() {
